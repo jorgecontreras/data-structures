e8dc4dbb22296d905906f2d33332abfa3a454f94
Jorge <jcontreras@pch.com>
2021-08-31 09:55:18 -0400

    add graph: adjacency list

---
+++ Graph.py
@@ -0,0 +1,33 @@
+# Adjacency List
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.V = defaultdict(list)
+
+    def addEdge(self, i, j):
+        self.V[i].append(j)
+
+    def display(self):
+        for v, e in self.V.items():
+            print(v)
+            print(e)
+
+graph = Graph()
+graph.addEdge(0, 4)
+graph.addEdge(0, 6)
+graph.addEdge(0, 7)
+
+graph.addEdge(1, 2)
+graph.addEdge(1, 3)
+graph.addEdge(1, 5)
+
+graph.addEdge(2, 6)
+graph.addEdge(2, 8)
+graph.addEdge(2, 4)
+graph.addEdge(2, 3)
+
+graph.display()
+
+
+    
